203f687dd2f851b35bb17e424ddb37357427f5da
Radhika Patel <radhika.patel2206@gmail.com>
2024-05-29 19:45:39 +0530

    fix get coregroups for reseller

--- core/views/coregroup.py
+++ core/views/coregroup.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from core.models import CoreGroup
+from core.models import CoreGroup, Organization
 from core.serializers import CoreGroupSerializer
 from core.permissions import IsOrgMember
 
@@ -22,7 +22,17 @@ class CoreGroupViewSet(viewsets.ModelViewSet):
         queryset = self.get_queryset()
         if not request.user.is_global_admin:
             q1 = queryset.filter(organization=None)
-            q2 = queryset.filter(organization_id=request.user.organization_id)
+            organization_id = request.user.organization_id
+            if request.user.is_org_admin:
+                reseller_orgs = [organization_id]
+                org = Organization.objects.get(pk=organization_id)
+                if org.is_reseller:
+                    for ro in org.reseller_customer_orgs:
+                        reseller_orgs.append(ro)
+
+                q2 = queryset.filter(organization_id__in=reseller_orgs)
+            else:
+                q2 = queryset.filter(organization_id=organization_id)
             queryset = q1.union(q2, all=True)
         return Response(self.get_serializer(queryset, many=True).data)
 
